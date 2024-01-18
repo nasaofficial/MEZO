539c2872e07ac43d16dce1fe58eb02f2fb7f956d
nasaofficial <121038071+nasaofficial@users.noreply.github.com>
2024-01-18 18:47:15 +0300

    Update MEZO.bat

--- MEZO.bat
+++ MEZO.bat
@@ -38,7 +38,7 @@ echo              #                                                             
 echo              #    1. Tasklist and Taskkill                 2. Tasklist with Exclusions and Taskkill        #
 echo              #                                                                                             #
 echo              #                                                                                             #
-echo              #    3. Taskkill iCUE.exe                     4. Multiple Taskkill commands                   #
+echo              #    3. Windows 11 Activation                 4. Multiple Taskkill commands                   #
 echo              #                                                                                             #
 echo              #                                                                                             #
 echo              #    5. Windows 10 Activation                 6. Microsoft Office Activation                  #
@@ -165,8 +165,27 @@ color b
 
 :Option3
 color e
-taskkill /F /IM "iCUE.exe"
-
+@echo off
+title Activate Windows 11 BY MOAZ ELSAYED (+966 54 206 0259) (ALL versions) for FREE&cls&echo =====================================================================================&echo #Project: Activating Microsoft software products for FREE without additional software&echo =====================================================================================&echo.&echo #Supported products:&echo - Windows 11 Home&echo - Windows 11 Professional&echo - Windows 11 Education&echo - Windows 11 Enterprise&echo.&echo.&echo ============================================================================&echo Activating your Windows...&cscript //nologo slmgr.vbs /ckms >nul&cscript //nologo slmgr.vbs /upk >nul&cscript //nologo slmgr.vbs /cpky >nul&set i=1&wmic os | findstr /I "enterprise" >nul
+if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43 >nul||cscript //nologo slmgr.vbs /ipk DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4 >nul||cscript //nologo slmgr.vbs /ipk YYVX9-NTFWV-6MDM3-9PT4T-4M68B >nul||cscript //nologo slmgr.vbs /ipk 44RPN-FTY23-9VTTB-MP9BX-T84FV >nul||cscript //nologo slmgr.vbs /ipk WNMTR-4C88C-JK8YV-HQ7T2-76DF9 >nul||cscript //nologo slmgr.vbs /ipk 2F77B-TNFGY-69QQF-B8YKP-D69TJ >nul||cscript //nologo slmgr.vbs /ipk DCPHK-NFMTC-H88MJ-PFHPY-QJ4BJ >nul||cscript //nologo slmgr.vbs /ipk QFFDN-GRT3P-VKWWX-X7T3R-8B639 >nul||cscript //nologo slmgr.vbs /ipk M7XTQ-FN8P6-TTKYV-9D4CC-J462D >nul||cscript //nologo slmgr.vbs /ipk 92NFX-8DJQP-P6BBQ-THF9C-7CG2H >nul&goto skms) else wmic os | findstr /I "home" >nul
+if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99 >nul||cscript //nologo slmgr.vbs /ipk 3KHY7-WNT83-DGQKR-F7HPR-844BM >nul||cscript //nologo slmgr.vbs /ipk 7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH >nul||cscript //nologo slmgr.vbs /ipk PVMJN-6DFY6-9CCP6-7BKTT-D3WVR >nul&goto skms) else wmic os | findstr /I "education" >nul
+if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk NW6C2-QMPVW-D7KKK-3GKT6-VCFB2 >nul||cscript //nologo slmgr.vbs /ipk 2WH4N-8QGBV-H22JP-CT43Q-MDWWJ >nul&goto skms) else wmic os | findstr /I "11 pro" >nul
+if %errorlevel% EQU 0 (cscript //nologo slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX >nul||cscript //nologo slmgr.vbs /ipk MH37W-N47XK-V7XM9-C7227-GCQG9 >nul||cscript //nologo slmgr.vbs /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J >nul||cscript //nologo slmgr.vbs /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF >nul||cscript //nologo slmgr.vbs /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y >nul||cscript //nologo slmgr.vbs /ipk YVWGF-BXNMC-HTQYQ-CPQ99-66QFC >nul&goto skms) else (goto notsupported)
+:skms
+if %i% GTR 10 goto busy
+if %i% EQU 1 set KMS=kms7.MSGuides.com
+if %i% EQU 2 set KMS=kms8.MSGuides.com
+if %i% EQU 3 set KMS=kms9.MSGuides.com
+if %i% GTR 3 goto ato
+cscript //nologo slmgr.vbs /skms %KMS%:1688 >nul
+:ato
+echo ============================================================================&echo.&echo.&cscript //nologo slmgr.vbs /ato | find /i "successfully" && (echo.&echo ============================================================================&echo.&echo #My official number: +966542060259&echo.&echo #How it works: it just does&echo.&echo #Please feel free to contact me at moazelsayed884@gmail.com if you have any questions or concerns.&echo.&echo #Please consider supporting this project: donate.msguides.com&echo.&echo ============================================================================&choice /n /c YN /m "Would you like to Contact me on telegram? [Y,N]?" & if errorlevel 2 exit) || (echo MOAZ SAYS: The connection to my KMS server failed! Trying to connect to another one... & echo MOAZ SAYS: Please wait... & echo. & echo. & set /a i+=1 & goto skms)
+explorer "https://t.me/id9p0"&goto halt
+:notsupported
+echo ============================================================================&echo.&echo MOAZ SAYS: Sorry, your version is not supported.&echo.&goto halt
+:busy
+echo ============================================================================&echo.&echo MOAZ SAYS: Sorry, the server is busy and can't respond to your request. Please try again.&echo.
+:halt
 timeout /nobreak /t 3 >nul
 echo Done...
 cls
