8622636e3d73c9a3a6c89da95a343700dc5967c7
MEZO <121038071+nasaofficial@users.noreply.github.com>
2024-03-11 17:41:10 +0300

    Update MEZO.bat

--- MEZO.bat
+++ MEZO.bat
@@ -386,7 +386,7 @@ choice /n /c 123456789 /m "Enter the number of your choice:"
 
 rem Check the user's choice and perform the corresponding action
 if errorlevel 9 goto HELP
-if errorlevel 8 goto soon1
+if errorlevel 8 goto menu4
 if errorlevel 7 goto menu2
 if errorlevel 6 goto DisableWifi
 if errorlevel 5 goto WifiFixer
@@ -400,7 +400,7 @@ if errorlevel 1 goto ManualServices
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-:MENU4
+:MENU5
 
 @echo off
     
